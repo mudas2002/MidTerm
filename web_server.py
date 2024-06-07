be9dc056590345cc9f459c2d6a13f158b12748a8
Mudassir Ali <mudassir45ali@gmail.com>
2024-06-07 13:29:49 +0000

    .....

--- web_server.py
+++ web_server.py
@@ -1,12 +1,12 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a simple Flask app by Mudassir."
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4512)
+    app.run(host="0.0.0.0", port=5000)
