d09eb838aa35450668e7ac02092b4356b021760a
Mudassir Ali <mudassir45ali@gmail.com>
2024-06-07 13:46:46 +0000

    Rep

---
+++ templates/gitadd.sh
@@ -0,0 +1,11 @@
+# Navigate to your project directory
+cd ~/midterm-test-repo
+
+# Stage all changes
+git add -A
+
+# Commit the changes
+git commit -m "Rep"
+
+# Push the changes to the remote repository
+git push
