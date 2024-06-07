cf8b105d588145d6a1eb6b111eb88cda1b11795a
Mudassir Ali <mudassir45ali@gmail.com>
2024-06-07 13:09:52 +0000

    mkl

--- web_server.py
+++ web_server.py
@@ -9,4 +9,4 @@ def home():
     return "Hello, World! This is a simple Flask app by Mudassir."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=4512)
